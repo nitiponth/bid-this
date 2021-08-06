32a67cf663776282fded509f48f0d5dd52f8cc6e
nitiponth <ntitiponth@outlook.com>
2021-08-06 16:29:16 +0700

    add backdrop layout for auth popup (only login done)

---
+++ components/auth/login.js
@@ -0,0 +1,64 @@
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__close-box">
+        <img
+          src="/images/SVG/cross.svg"
+          alt="clost button"
+          className="btn__close-img"
+        />
+      </div>
+      <div className="login__logo">
+        <div className="login__logo-box">
+          <img
+            src="/images/logo-square.png"
+            alt="logo"
+            className="login__logo-img"
+          />
+        </div>
+        <h1 className="login__logo-text">Welcome</h1>
+      </div>
+
+      <div className="login__content">
+        <form className="login__form">
+          <label htmlFor="email" className="login__form--text">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email Address"
+            className="login__form-input"
+          />
+          <label htmlFor="password" className="login__form--text">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            className="login__form-input"
+          />
+          <a href="#" className="login__link login__link--forgot">
+            Forgot password?
+          </a>
+
+          <button type="submit" className="login__btn">
+            Login
+          </button>
+        </form>
+      </div>
+      <div className="login__footer">
+        Don't have an account?
+        <a
+          href="#"
+          className="login__link login__link--sign footer login__footer--link"
+        >
+          Sign up
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import Login from "../components/auth/login";
+import Backdrop from "../components/layout/backdrop";
 import Layout from "../components/layout/layout";
 
 export default function Home() {
   return (
     <Fragment>
       <Head>
-        <title>Banana Bid</title>
+        <title>Bid This</title>
         <meta
           name="description"
           content="Buy the product at the price you choose."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Backdrop>
+        <Login />
+      </Backdrop>
       <Layout />
     </Fragment>
   );
