2adca9c0efa0727b32c955bdb7c11d0210535479
nitiponth <ntitiponth@outlook.com>
2022-01-23 21:56:34 +0700

    take sentry out -2

--- pages/api/s3-upload.js
+++ pages/api/s3-upload.js
@@ -1,13 +1,10 @@
-import { withSentry } from "@sentry/nextjs";
 import { APIRoute } from "next-s3-upload";
 import { v4 as uuidv4 } from "uuid";
 
-export default withSentry(
-  APIRoute.configure({
-    key(req, filename) {
-      let name = filename.replace(/\s/g, "-");
-      name = uuidv4().concat(`-${name}`);
-      return `images/${name}`;
-    },
-  })
-);
+export default APIRoute.configure({
+  key(req, filename) {
+    let name = filename.replace(/\s/g, "-");
+    name = uuidv4().concat(`-${name}`);
+    return `images/${name}`;
+  },
+});
